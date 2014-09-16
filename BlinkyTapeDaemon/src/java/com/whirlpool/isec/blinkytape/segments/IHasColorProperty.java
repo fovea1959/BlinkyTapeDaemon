6d4b100491fe62d72a09f1ef833364c52232ddd8
fovea1959 <dwegscheid@sbcglobal.net>
2014-09-15 21:40:44 -0400

    Commit missing bits.

---
+++ BlinkyTapeDaemon/src/java/com/whirlpool/isec/blinkytape/segments/IHasColorProperty.java
@@ -0,0 +1,7 @@
+package com.whirlpool.isec.blinkytape.segments;
+
+import java.awt.Color;
+
+public interface IHasColorProperty {
+  public Color getColor();
+}
---
+++ BlinkyTapeDaemon/src/java/com/whirlpool/isec/blinkytape/segments/IHasNumericValueProperty.java
@@ -0,0 +1,5 @@
+package com.whirlpool.isec.blinkytape.segments;
+
+public interface IHasNumericValueProperty {
+  public Double getValue();
+}
